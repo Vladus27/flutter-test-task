ec560530fbe3acc0980cdd3c4251cb74e7405e55
Vlad <hellomyvolodia@outlook.com>
2026-03-12 01:24:23 +0200

    use HomeColorChanger in HomeScreen

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_task/features/home/widgets/home_color_changer.dart';
 
 ///Screen that allows the user to change the background color
 class HomeScreen extends StatelessWidget {
   ///Default constructor for HomeScreen
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello there'),
-      ),
-    );
+    return const HomeColorChanger();
   }
 }
