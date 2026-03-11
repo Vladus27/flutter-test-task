d3326583aef65e21ae09e77ff1a2169742d4bb85
Vlad <hellomyvolodia@outlook.com>
2026-03-12 01:18:21 +0200

    add widget to handle background color changes

---
+++ lib/features/home/widgets/home_color_changer.dart
@@ -0,0 +1,43 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test_task/features/home/widgets/home_content.dart';
+
+/// Widget that allows the user to change the background color
+class HomeColorChanger extends StatefulWidget {
+  /// Default constructor for HomeColorChanger
+  const HomeColorChanger({
+    super.key,
+  });
+
+  @override
+  State<HomeColorChanger> createState() => _HomeColorChangerState();
+}
+
+class _HomeColorChangerState extends State<HomeColorChanger> {
+  Color? _color;
+  final _random = math.Random();
+  static const int _maxAlphaValue = 255; // oppacity
+  static const int _maxColorChannel = 256; // exclusive upper bound for channels
+
+  Color _getRandomColor() {
+    return Color.fromARGB(
+      _maxAlphaValue,
+      _random.nextInt(_maxColorChannel),
+      _random.nextInt(_maxColorChannel),
+      _random.nextInt(_maxColorChannel),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: HomeContent(color: _color),
+      onTap: () {
+        setState(() {
+          _color = _getRandomColor();
+        });
+      },
+    );
+  }
+}
