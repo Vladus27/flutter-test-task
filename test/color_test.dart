94e9922ba874bdb01ebae01d0b861ccd09d40b41
Vlad <hellomyvolodia@outlook.com>
2026-03-12 19:21:13 +0200

    Avoid using magic numbers in unit test

--- test/color_test.dart
+++ test/color_test.dart
@@ -3,17 +3,30 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_test_task/features/home/utils/color_generator.dart';
 
+const int maxColorChannel = 255;
+const int minColorChannel = 0;
+
+int channel(double value) =>
+    (value * maxColorChannel).round().clamp(minColorChannel, maxColorChannel);
+
 void main() {
   test('generated color has valid values', () {
     final random = math.Random();
 
     final color = generateRandomColor(random);
 
-    expect(channel(color.a), 255);
-    expect(channel(color.r), inInclusiveRange(0, 255));
-    expect(channel(color.g), inInclusiveRange(0, 255));
-    expect(channel(color.b), inInclusiveRange(0, 255));
+    expect(channel(color.a), maxColorChannel);
+    expect(
+      channel(color.r),
+      inInclusiveRange(minColorChannel, maxColorChannel),
+    );
+    expect(
+      channel(color.g),
+      inInclusiveRange(minColorChannel, maxColorChannel),
+    );
+    expect(
+      channel(color.b),
+      inInclusiveRange(minColorChannel, maxColorChannel),
+    );
   });
 }
-
-int channel(double value) => (value * 255).round().clamp(0, 255);
